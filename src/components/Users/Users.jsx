189a38391757648f14a3fe9fb17cf4c719b1444b
Sveta <paranyushkina.sv@gmail.com>
2022-04-10 15:57:39 +0300

    users-ajax

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,47 @@
+import React from 'react';
+import userPhoto from '../../assets/images/user.png';
 import s from './Users.module.css';
-import * as axios from 'axios';
-import userPhoto from '../../assets/images/user.png'
 
+const Users = (props) => {
+	// let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-let Users = (props) => {
-	let getUsers = () => {
-		if(props.users.length === 0) {
-			axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-				props.setUsers(response.data.items)
-			});
-		}
+	let pages = [];
+	for (let i =1; i < 11; i++) {
+		pages.push(i);
 	}
-	
-
-	return <div>
-		<button onClick={getUsers}>get users</button>
+ return (
+	<div>
+		<div>
+			{pages.map(p => {
+				return <span className={props.currentPage === p ? s.selectedPage : undefined} onClick={()=> {props.onPageChanged(p)}}>{p}</span>
+			})}
+		</div>
 	{
-		props.users.map(u => <div key={u.id}>
+	props.users.map(u => <div key={u.id}>
+		<span>
+			<div>
+				<img className={s.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
+			</div>
+			<div>
+				{u.followed 
+				? <button onClick={()=> {props.unfollow(u.id)}}>Unfollow</button> 
+				: <button onClick={()=> {props.follow(u.id)}}>Follow</button>}
+			</div>
+		</span>
+		<span>
 			<span>
-				<div>
-					<img className={s.userPhoto} src={u.photos.small != null ? u.photo.small : userPhoto} alt="" />
-				</div>
-				<div>
-					{u.followed 
-					? <button onClick={()=> {props.unfollow(u.id)}}>Unfollow</button> 
-					: <button onClick={()=> {props.follow(u.id)}}>Follow</button>}
-				</div>
+				<div>{u.name}</div>
+				<div>{u.status}</div>
 			</span>
 			<span>
-				<span>
-					<div>{u.name}</div>
-					<div>{u.status}</div>
-				</span>
-				<span>
-					<div>{"u.location.country"}</div>
-					<div>{"u.location.city"}</div>
-				</span>
+				<div>{"u.location.country"}</div>
+				<div>{"u.location.city"}</div>
 			</span>
-		</div>)
-		
+		</span>
+	</div>)
 	}	
 	</div>	
+ )
 }
 
 export default Users;
---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,7 @@
+import preloader from '../../../assets/images/spinner.svg'
+
+let Preloader = () => {
+ return <div style={{backgroundColor: 'white'}}><img srs={preloader}  alt=''/></div>
+}
+
+export default Preloader;
